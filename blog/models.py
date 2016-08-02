28f51dc32f385e69091db2e5d7f141fcc313c927
Luke Rickard <luke.x.rickard@gmail.com>
2016-08-02 13:44:43 +0100

    adding basic html, models and views

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+
+class Post(models.Model):
+    external_id = models.CharField(max_length=32, null=False, unique=True)
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_on = models.DateTimeField(default=timezone.now)
+    published_on = models.DateTimeField(blank=True, null=True)
+
+    def __init__(self):
+        self.externalId = str(uuid.uuid4())
+
+    def publish(self):
+        self.publishedOn = timezone.now()
+        self.save()
